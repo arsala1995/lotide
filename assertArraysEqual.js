7e3b39f6299cda57c7d1fd51c00db55eb9c0aec2
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-21 15:23:06 -0700

    editing assertArrayEqaul file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,27 +12,25 @@ const assertArraysEqual = function(actual) {
 };
 
 
-const eqArrays = function(Array1, Array2){
+// const eqArrays = function(Array1, Array2){
 
-        for( var i = 0; i < Array1.length; i++){
-        for( var j = i; j < Array2.length; j++){
+//         for( var i = 0; i < Array1.length; i++){
+//         for( var j = i; j < Array2.length; j++){
 
-       if(Array1[i] != Array2[j]){
-        return false;
-}
+//        if(Array1[i] != Array2[j]){
+//         return false;
+// }
 
-        else{
-        return true;
-}
-}
-}
+//         else{
+//         return true;
+// }
+// }
+// }
 
-};
+// };
+
+module.exports = assertArraysEqual;
 
 
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
-
-assertArraysEqual(eqArrays([1, 2, 3], [4, 2, 1]));
 
 
