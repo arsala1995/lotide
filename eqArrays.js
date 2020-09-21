a5add30d8bbc2ad3da9f654d8ea72f462a7aee8e
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-21 15:05:38 -0700

    editing eqArrays to use test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-        if(actual !== expected){
-console.log("Assertion Failed: " + actual + " !== "+ expected );
-}
 
-        else{
-        console.log("Assertion Passed: " + actual + " === "+ expected );
-
-}
-};
 
 
 const eqArrays = function(Array1, Array2){
@@ -28,10 +19,4 @@ const eqArrays = function(Array1, Array2){
 };
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+module.exports = eqArrays;
