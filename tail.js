e71ac479e2d20a742bcf89903095e73e8009132e
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-21 14:58:43 -0700

    editing tail file to make it run

--- tail.js
+++ tail.js
@@ -1,28 +1,21 @@
-const assertEqual = function(actual2, expected) {
-        for (var i = 0; i < actual2.length; i++){
-
-	for ( var j = 0; j < expected.length; j++){
-
-	if(actual2[i] !== expected[j]){
-	console.log("Assertion Failed: " + actual2 + " !== "+ expected );
-}
-
-        else{
-        console.log("Assertion Passed: " + actual2 + " === "+ expected );
-
-
-}
-}
-}
+const assertEqual = function(actual, expected) {
+	if(actual !== expected){
+		console.log("Assertion Failed: " + actual + " !== "+ expected );
+		}
+		
+			else{
+			console.log("Assertion Passed: " + actual + " === "+ expected );
+		
+		}
 };
 
 
 const tail = function(actual1){
-var tail = actual1.slice(1);
+var tail1 = actual1.slice(1).join(" ");
 
-return tail;
+
+return tail1;
 }
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+module.exports = tail;
 
