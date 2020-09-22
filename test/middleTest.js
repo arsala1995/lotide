0c4fe61dc4e318fdefb6394ecec611d25da5feb1
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-21 17:07:34 -0700

    editing test files to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+
 
 const middle  = require('../middle');
 
-assertArraysEqual(middle([1]));
-assertArraysEqual(middle([1, 2]));
-assertArraysEqual(middle([1, 2, 3]));
-assertArraysEqual(middle([1, 2, 3, 4]));
+
+const assert = require('chai').assert;
+
+
+
+
+describe("#middle", () => {
+  it("returns [ 2 ] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [ 2, 3 ] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,15 @@
 
-const assertEqual = require('../assertEqual');
 
+const assert = require('chai').assert;
 const tail  = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, "Lighthouse Labs"); 
+
+
+describe("#tail", () => {
+  it("returns Lighthouse Labs for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse Labs");
+  });
+
+  
+
+});
